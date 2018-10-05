9c6239956f7a758c22c69ea41b0c20c3df5b5e05
Abdul Dakkak <abduld@wolfram.com>
2018-10-05 12:19:28 -0500

    do not error if cannot parse or create ratelimit

--- ratelimit.go
+++ ratelimit.go
@@ -26,9 +26,8 @@ func New(opts ...RateLimitOption) error {
 	}
 	tmpDir := os.TempDir()
 	if !com.IsDir(tmpDir) {
-		msg := "Not able to find temporary directory for " + config.App.Name + "."
-		log.Debug(msg)
-		return errors.New(msg)
+		log.Debugf("Unable to find temporary directory for %s.", config.App.Name)
+		return nil
 	}
 	dotKeepFilePath := filepath.Join(tmpDir, config.App.Name+".keep")
 	if !com.IsFile(dotKeepFilePath) {
@@ -37,20 +36,18 @@ func New(opts ...RateLimitOption) error {
 	}
 	tbytes, err := ioutil.ReadFile(dotKeepFilePath)
 	if err != nil {
-		msg := "Not able to read " + dotKeepFilePath + "."
-		log.WithError(err).Debug(msg)
-		return errors.Wrap(err, msg)
+		log.WithError(err).Debugf("Unable to read %s.", dotKeepFilePath)
+		return nil
 	}
 	prevTime, err := time.Parse(timeFormat, string(tbytes))
 	if err != nil {
-		msg := "Not able to parse time in " + dotKeepFilePath + "."
-		log.WithError(err).Debug(msg)
-		return errors.Wrap(err, msg)
+		log.WithError(err).Debugf("Unable to parse time in %s.", dotKeepFilePath)
+		return nil
 	}
 	timeDiff := time.Since(prevTime)
 	if timeDiff < options.limit {
 		msg := "Last submission was made " + humanize.Time(prevTime) + ". " +
-			"Due to the rate limitter, submissions are not allows within a " + options.limit.String() +
+			"Due to the rate limit, submissions are not allows within a " + options.limit.String() +
 			"  time window. "
 		log.Debug(msg)
 		return errors.New(msg)
